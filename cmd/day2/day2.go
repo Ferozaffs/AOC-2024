18f38ebe4e0c62edd25fecfdaffb74de535d2974
root <feroza_ffs@hotmail.com>
2024-12-02 21:16:49 +0100

    completed day 2

--- cmd/day2/day2.go
+++ cmd/day2/day2.go
@@ -3,7 +3,10 @@ package day2
 import (
 	"aoc2024/cmd"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -30,6 +33,64 @@ func Run1() {
 }
 
 func Solve(data string) (int, int) {
+	lines := strings.Split(data, "\n")
 
-	return 0, 0
+	ans1 := 0
+	ans2 := 0
+	for _, line := range lines {
+		parts := strings.Fields(line)
+
+		var numbers []int
+		for _, part := range parts {
+			num, _ := strconv.Atoi(part)
+			numbers = append(numbers, num)
+		}
+
+		if Sequence(numbers, 0) {
+			ans1++
+		}
+		if SequenceTolerance(numbers) {
+			ans2++
+		}
+	}
+
+	return ans1, ans2
+}
+
+func SequenceTolerance(numbers []int) bool {
+	if Sequence(numbers, 0) {
+		return true
+	} else {
+		for i := range numbers {
+			prefix := append([]int{}, numbers[:i]...)
+			newSequence := append(prefix, numbers[i+1:]...)
+
+			if Sequence(newSequence, 0) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func Sequence(numbers []int, state int) bool {
+	first, numbers := numbers[0], numbers[1:]
+
+	if len(numbers) == 0 {
+		return true
+	}
+
+	delta := first - numbers[0]
+	deltaAbs := int(math.Abs(float64(delta)))
+
+	if deltaAbs > 0 && deltaAbs < 4 {
+		if delta > 0 && (state == 1 || state == 0) {
+			return Sequence(numbers, 1)
+		} else if delta < 0 && (state == -1 || state == 0) {
+			return Sequence(numbers, -1)
+		}
+	}
+
+	return false
 }
--- cmd/day2/day2_test.go
+++ cmd/day2/day2_test.go
@@ -5,9 +5,19 @@ import (
 )
 
 func TestSample1(t *testing.T) {
+	sampleData := `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+9 8 9 7 6`
 
-}
-
-func TestSample2(t *testing.T) {
-
+	ans1, ans2 := Solve(sampleData)
+	if ans1 != 2 {
+		t.Fatalf("Mismatch! Expected 2 got %d", ans1)
+	}
+	if ans2 != 5 {
+		t.Fatalf("Mismatch! Expected 5 got %d", ans2)
+	}
 }
