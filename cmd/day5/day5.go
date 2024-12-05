338098ffaf603b6aed3665cf3debcc9c72c52d36
root <feroza_ffs@hotmail.com>
2024-12-05 18:46:33 +0100

    completed day 5

--- cmd/day5/day5.go
+++ cmd/day5/day5.go
@@ -4,6 +4,9 @@ import (
 	"aoc2024/cmd"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -30,6 +33,80 @@ func Run1() {
 }
 
 func Solve(data string) (int, int) {
+	ans1 := 0
+	ans2 := 0
 
-	return 0, 0
+	pageMap := make(map[string][]string)
+
+	stage := 0
+
+	lines := strings.Split(data, "\n")
+	for _, l := range lines {
+		if len(l) == 0 {
+			stage++
+			continue
+		}
+
+		if stage == 0 {
+			s := strings.Split(l, "|")
+			pageMap[s[0]] = append(pageMap[s[0]], s[1])
+		} else {
+			values := strings.Split(l, ",")
+			if CheckSequence(values, pageMap) {
+				mv, _ := strconv.Atoi(values[len(values)/2])
+				ans1 += mv
+			} else {
+				sortedValues := SortList(values, pageMap)
+
+				mv, _ := strconv.Atoi(sortedValues[len(sortedValues)/2])
+				ans2 += mv
+			}
+		}
+	}
+
+	return ans1, ans2
+}
+
+func CheckSequence(values []string, pageMap map[string][]string) bool {
+	var parsedValues []string
+	for _, v := range values {
+		p, ok := pageMap[v]
+		if ok {
+			for _, pn := range p {
+				if slices.Contains(parsedValues, pn) {
+					return false
+				}
+			}
+		}
+
+		parsedValues = append(parsedValues, v)
+	}
+
+	return true
+}
+
+func SortList(values []string, pageMap map[string][]string) []string {
+	var sortedList []string
+
+	for _, v := range values {
+		p := pageMap[v]
+
+		found := false
+	out:
+		for i := range sortedList {
+			for _, pn := range p {
+				if pn == sortedList[i] {
+					sortedList = append(sortedList[:i], append([]string{v}, sortedList[i:]...)...)
+					found = true
+					break out
+				}
+			}
+		}
+
+		if !found {
+			sortedList = append(sortedList, v)
+		}
+	}
+
+	return sortedList
 }
--- cmd/day5/day5_test.go
+++ cmd/day5/day5_test.go
@@ -4,10 +4,41 @@ import (
 	"testing"
 )
 
-func TestSample1(t *testing.T) {
+func TestSample(t *testing.T) {
+	sampleData := `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
 
-}
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
 
-func TestSample2(t *testing.T) {
-
+	ans1, ans2 := Solve(sampleData)
+	if ans1 != 143 {
+		t.Fatalf("Mismatch! Expected 143 got %d", ans1)
+	}
+	if ans2 != 123 {
+		t.Fatalf("Mismatch! Expected 123 got %d", ans2)
+	}
 }
