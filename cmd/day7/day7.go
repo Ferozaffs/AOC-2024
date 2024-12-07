20e068d470f66b7a1f7c967c895d3d4719cc6be8
root <feroza_ffs@hotmail.com>
2024-12-07 12:09:42 +0100

    completed day 7

--- cmd/day7/day7.go
+++ cmd/day7/day7.go
@@ -3,7 +3,10 @@ package day7
 import (
 	"aoc2024/cmd"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -30,6 +33,81 @@ func Run1() {
 }
 
 func Solve(data string) (int, int) {
+	lines := strings.Split(data, "\n")
 
-	return 0, 0
+	ans1 := 0
+	ans2 := 0
+	for _, line := range lines {
+		{
+			r, s := TestEquation(line, false)
+			if s {
+				ans1 += r
+			}
+		}
+		{
+			r, s := TestEquation(line, true)
+			if s {
+				ans2 += r
+			}
+		}
+	}
+
+	return ans1, ans2
+}
+
+func TestEquation(data string, useBase3 bool) (int, bool) {
+	s := strings.Split(data, ":")
+	result, _ := strconv.Atoi(s[0])
+
+	numberStrings := strings.Fields(s[1])
+
+	numbers := []int{}
+	for _, ns := range numberStrings {
+		n, _ := strconv.Atoi(ns)
+		numbers = append(numbers, n)
+	}
+
+	permutations := 0
+	if useBase3 {
+		permutations = int(math.Pow(3.0, float64(len(numbers)-1)))
+	} else {
+		permutations = int(math.Pow(2.0, float64(len(numbers)-1)))
+	}
+
+	for i := 0; i < permutations; i++ {
+		bs := ""
+
+		if useBase3 {
+			bs = ToBase3(len(numbers)-1, i)
+		} else {
+			bs = fmt.Sprintf("%0*b", len(numbers)-1, i)
+		}
+
+		sum := numbers[0]
+		for j := 1; j < len(numbers); j++ {
+			if useBase3 && j <= len(bs) && bs[j-1] == '2' {
+				concat := fmt.Sprintf("%d%d", sum, numbers[j])
+				sum, _ = strconv.Atoi(concat)
+			} else if j <= len(bs) && bs[j-1] == '1' {
+				sum *= numbers[j]
+			} else {
+				sum += numbers[j]
+			}
+		}
+
+		if sum == result {
+			return result, true
+		}
+	}
+
+	return result, false
+}
+
+func ToBase3(numOperator int, index int) string {
+	b3 := ""
+	for index > 0 {
+		b3 = fmt.Sprintf("%d%s", index%3, b3)
+		index /= 3
+	}
+	return strings.Repeat("0", numOperator-len(b3)) + b3
 }
--- cmd/day7/day7_test.go
+++ cmd/day7/day7_test.go
@@ -4,10 +4,22 @@ import (
 	"testing"
 )
 
-func TestSample1(t *testing.T) {
+func TestSample(t *testing.T) {
+	sampleData := `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
 
-}
-
-func TestSample2(t *testing.T) {
-
+	ans1, ans2 := Solve(sampleData)
+	if ans1 != 3749 {
+		t.Fatalf("Mismatch! Expected 3749 got %d", ans1)
+	}
+	if ans2 != 11387 {
+		t.Fatalf("Mismatch! Expected 11387 got %d", ans2)
+	}
 }
