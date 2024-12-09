f48d63d8d7980eed8b91cdc52240155b287e4e1c
root <feroza_ffs@hotmail.com>
2024-12-09 21:04:27 +0100

    completed day 9

--- cmd/day9/day9.go
+++ cmd/day9/day9.go
@@ -3,6 +3,7 @@ package day9
 import (
 	"aoc2024/cmd"
 	"fmt"
+	"math"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -30,6 +31,154 @@ func Run1() {
 }
 
 func Solve(data string) (int, int) {
+	extractedValues := [][]int{}
+	extractedFree := [][]int{}
+	index := 0
+	for i, r := range data {
+		v := int(r - '0')
 
-	return 0, 0
+		var values []int
+		for j := 0; j < v; j++ {
+			if i%2 == 0 {
+				values = append(values, index)
+			} else {
+				values = append(values, -1)
+			}
+		}
+
+		if i%2 == 0 {
+			extractedValues = append(extractedValues, values)
+		} else {
+			extractedFree = append(extractedFree, values)
+			index++
+		}
+	}
+
+	combined := MoveAndCombine(DeepCopy(extractedValues), DeepCopy(extractedFree), false)
+
+	ans1 := 0
+	counter := 0
+out2:
+	for _, s := range combined {
+		for _, v := range s {
+			if v == -1 {
+				break out2
+			}
+
+			ans1 += counter * v
+			counter++
+		}
+	}
+
+	combined = MoveAndCombine(DeepCopy(extractedValues), DeepCopy(extractedFree), true)
+
+	ans2 := 0
+	counter = 0
+	for _, s := range combined {
+		for _, v := range s {
+			if v == -1 {
+				counter++
+				continue
+			}
+
+			ans2 += counter * v
+			counter++
+		}
+	}
+
+	return ans1, ans2
+}
+
+func MoveAndCombine(extractedValues [][]int, extractedFree [][]int, smart bool) [][]int {
+	var combined [][]int
+
+	if !smart {
+		indexSpace := 0
+		indexValue := 0
+	out:
+		for i := len(extractedValues) - 1; i >= 0; i-- {
+			for j := len(extractedValues[i]) - 1; j >= 0; j-- {
+				if indexSpace >= i {
+					break out
+				}
+
+				extractedFree[indexSpace][indexValue] = extractedValues[i][j]
+				indexValue++
+				if indexValue == len(extractedFree[indexSpace]) {
+					for {
+						indexSpace++
+						indexValue = 0
+						if indexSpace == len(extractedFree) {
+							break out
+						}
+						if extractedFree[indexSpace] != nil && len(extractedFree[indexSpace]) != 0 {
+							break
+						}
+					}
+
+				}
+
+				extractedValues[i] = append(extractedValues[i][:j], extractedValues[i][j+1:]...)
+			}
+			if len(extractedValues[i]) == 0 {
+				extractedValues = append(extractedValues[:i], extractedValues[i+1:]...)
+			}
+		}
+	} else {
+		for i := len(extractedValues) - 1; i >= 0; i-- {
+			for j, f := range extractedFree {
+				if j < i {
+					count := 0
+					offset := 0
+					for _, v := range f {
+						if v == -1 {
+							count++
+						} else {
+							offset++
+						}
+					}
+
+					if count >= len(extractedValues[i]) {
+						numToAppend := 0
+						for k := 0; k < len(extractedValues[i]); {
+							f[offset] = extractedValues[i][k]
+							offset++
+							numToAppend++
+							extractedValues[i] = append(extractedValues[i][:k], extractedValues[i][k+1:]...)
+						}
+
+						for a := 0; a < numToAppend; a++ {
+							if i-1 < len(extractedFree) {
+								extractedFree[i-1] = append(extractedFree[i-1], -1)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	maxLen := int(math.Max(float64(len(extractedValues)), float64(len(extractedFree))))
+
+	for i := 0; i < maxLen; i++ {
+		if i < len(extractedValues) {
+			combined = append(combined, extractedValues[i])
+		}
+		if i < len(extractedFree) && extractedFree[i] != nil {
+			combined = append(combined, extractedFree[i])
+		}
+	}
+
+	return combined
+}
+
+func DeepCopy(original [][]int) [][]int {
+	result := make([][]int, len(original))
+
+	for i, inner := range original {
+		result[i] = make([]int, len(inner))
+		copy(result[i], inner)
+	}
+
+	return result
 }
--- cmd/day9/day9_test.go
+++ cmd/day9/day9_test.go
@@ -4,10 +4,37 @@ import (
 	"testing"
 )
 
-func TestSample1(t *testing.T) {
+func TestSampleSimple(t *testing.T) {
+	sampleData := `12345`
+
+	ans1, ans2 := Solve(sampleData)
+	if ans1 != 60 {
+		t.Fatalf("Mismatch! Expected 60 got %d", ans1)
+	}
+	if ans2 != 132 {
+		t.Fatalf("Mismatch! Expected 132 got %d", ans2)
+	}
 
 }
 
-func TestSample2(t *testing.T) {
+func TestSampleExtra(t *testing.T) {
+	sampleData := `354631466260`
 
+	_, ans2 := Solve(sampleData)
+	if ans2 != 1325 {
+		t.Fatalf("Mismatch! Expected 1325 got %d", ans2)
+	}
+
+}
+
+func TestSample(t *testing.T) {
+	sampleData := `2333133121414131402`
+
+	ans1, ans2 := Solve(sampleData)
+	if ans1 != 1928 {
+		t.Fatalf("Mismatch! Expected 1928 got %d", ans1)
+	}
+	if ans2 != 2858 {
+		t.Fatalf("Mismatch! Expected 2858 got %d", ans2)
+	}
 }
