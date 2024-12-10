9d01775fb96048d03bcad743fff699c7efa8aad5
root <feroza_ffs@hotmail.com>
2024-12-10 19:14:32 +0100

    completed day 10

--- cmd/day10/day10.go
+++ cmd/day10/day10.go
@@ -2,11 +2,19 @@ package day10
 
 import (
 	"aoc2024/cmd"
+	"aoc2024/helpers"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
+
+var directions = []helpers.Point{
+	{1, 0},
+	{0, 1},
+	{0, -1},
+	{-1, 0},
+}
 
 var day1_1Cmd = &cobra.Command{
 	Use:   "day10",
@@ -30,6 +38,46 @@ func Run1() {
 }
 
 func Solve(data string) (int, int) {
+	var grid helpers.Grid
+	grid.Init(data)
 
-	return 0, 0
+	ans1 := 0
+	ans2 := 0
+	for x := range grid {
+		for y := range grid[x] {
+			if grid[x][y] == '0' {
+				paths := Search(helpers.Point{X: x, Y: y}, 0, &grid)
+				ans1 += FindUniquePaths(paths)
+				ans2 += len(paths)
+			}
+		}
+	}
+
+	return ans1, ans2
+}
+
+func Search(p helpers.Point, index int, g *helpers.Grid) []helpers.Point {
+	if index == 9 {
+		return []helpers.Point{p}
+	}
+
+	result := []helpers.Point{}
+	for i := 0; i < 4; i++ {
+		np := helpers.Point{X: p.X + directions[i].X, Y: p.Y + directions[i].Y}
+
+		r, b := g.GetPoint(np)
+		if b && int(r-'0') == index+1 {
+			result = append(result, Search(np, index+1, g)...)
+		}
+	}
+
+	return result
+}
+
+func FindUniquePaths(p []helpers.Point) int {
+	uniqueMap := make(map[helpers.Point]bool)
+	for _, v := range p {
+		uniqueMap[v] = true
+	}
+	return len(uniqueMap)
 }
--- cmd/day10/day10_test.go
+++ cmd/day10/day10_test.go
@@ -4,10 +4,21 @@ import (
 	"testing"
 )
 
-func TestSample1(t *testing.T) {
+func TestSample(t *testing.T) {
+	sampleData := `89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`
 
-}
-
-func TestSample2(t *testing.T) {
-
+	ans1, ans2 := Solve(sampleData)
+	if ans1 != 36 {
+		t.Fatalf("Mismatch! Expected 36 got %d", ans1)
+	}
+	if ans2 != 81 {
+		t.Fatalf("Mismatch! Expected 81 got %d", ans2)
+	}
 }
