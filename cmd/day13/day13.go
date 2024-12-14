3c254014290ae67a03ce5528de351ac6570a1eab
root <feroza_ffs@hotmail.com>
2024-12-14 16:24:22 +0100

    completed day 13, cramer rules!

--- cmd/day13/day13.go
+++ cmd/day13/day13.go
@@ -2,11 +2,21 @@ package day13
 
 import (
 	"aoc2024/cmd"
+	"aoc2024/helpers"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
+
+type Machine struct {
+	a helpers.Point
+	b helpers.Point
+	d helpers.Point
+}
 
 var day1_1Cmd = &cobra.Command{
 	Use:   "day13",
@@ -30,6 +40,77 @@ func Run1() {
 }
 
 func Solve(data string) (int, int) {
+	lines := strings.Split(data, "\n")
+	var machines []Machine
 
-	return 0, 0
+	for i := 0; i < len(lines); {
+		if len(lines[i]) == 0 {
+			i++
+			continue
+		}
+
+		aLine := lines[i]
+		bLine := lines[i+1]
+		dLine := lines[i+2]
+
+		a := Extract(aLine)
+		b := Extract(bLine)
+		d := Extract(dLine)
+
+		machines = append(machines, Machine{
+			a: a,
+			b: b,
+			d: d,
+		})
+
+		i += 3
+	}
+
+	ans1 := 0
+	for _, m := range machines {
+		a, b := CramersRule(m.a.X, m.a.Y, m.b.X, m.b.Y, m.d.X, m.d.Y)
+
+		if a > 0 && b > 0 {
+			ans1 += a*3 + b
+		}
+	}
+
+	ans2 := 0
+	for _, m := range machines {
+		a, b := CramersRule(m.a.X, m.a.Y, m.b.X, m.b.Y, m.d.X+10000000000000, m.d.Y+10000000000000)
+
+		if a > 0 && b > 0 {
+			ans2 += a*3 + b
+		}
+	}
+
+	return ans1, ans2
+}
+
+func Extract(s string) helpers.Point {
+	reQuery := `\d+`
+	re := regexp.MustCompile(reQuery)
+	matches := re.FindAllString(s, -1)
+
+	x, _ := strconv.Atoi(matches[0])
+	y, _ := strconv.Atoi(matches[1])
+
+	return helpers.Point{X: x, Y: y}
+}
+
+func CramersRule(ax int, ay int, bx int, by int, dx int, dy int) (int, int) {
+	det := ax*by - ay*bx
+
+	if det == 0 {
+		return -1, -1
+	}
+
+	d1 := dx*by - dy*bx
+	d2 := ax*dy - ay*dx
+
+	if d1%det != 0 || d2%det != 0 {
+		return -1, -1
+	}
+
+	return d1 / det, d2 / det
 }
--- cmd/day13/day13_test.go
+++ cmd/day13/day13_test.go
@@ -4,10 +4,28 @@ import (
 	"testing"
 )
 
-func TestSample1(t *testing.T) {
+func TestSample(t *testing.T) {
+	sampleData := `Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
 
-}
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
 
-func TestSample2(t *testing.T) {
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
 
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279`
+
+	ans1, _ := Solve(sampleData)
+	if ans1 != 480 {
+		t.Fatalf("Mismatch! Expected 480 got %d", ans1)
+	}
+	//if ans2 != 1206 {
+	//	t.Fatalf("Mismatch! Expected 1206 got %d", ans2)
+	//}
 }
