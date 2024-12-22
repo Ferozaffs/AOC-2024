5002b58e6b97be7a56be78672d5aaa343f4b8e5d
root <feroza_ffs@hotmail.com>
2024-12-22 15:58:30 +0100

    completed day 22

--- cmd/day22/day22.go
+++ cmd/day22/day22.go
@@ -4,6 +4,8 @@ import (
 	"aoc2024/cmd"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -30,6 +32,44 @@ func Run1() {
 }
 
 func Solve(data string) (int, int) {
+	lines := strings.Split(data, "\n")
 
-	return 0, 0
+	ans1 := 0
+	bananas := make(map[[4]int]int)
+	for _, s := range lines {
+		seen := make(map[[4]int]bool)
+		seq := [4]int{999, 999, 999, 999}
+
+		n, _ := strconv.Atoi(s)
+		for i := 0; i < 2000; i++ {
+			p := n % 10
+
+			n = (n ^ (n * 64)) % 16777216
+			n = (n ^ (n / 32)) % 16777216
+			n = (n ^ (n * 2048)) % 16777216
+
+			c := n % 10
+
+			seq[0] = seq[1]
+			seq[1] = seq[2]
+			seq[2] = seq[3]
+			seq[3] = c - p
+
+			if !seen[seq] {
+				seen[seq] = true
+				bananas[seq] += c
+			}
+		}
+
+		ans1 += n
+	}
+
+	ans2 := 0
+	for _, v := range bananas {
+		if v > ans2 {
+			ans2 = v
+		}
+	}
+
+	return ans1, ans2
 }
--- cmd/day22/day22_test.go
+++ cmd/day22/day22_test.go
@@ -4,10 +4,26 @@ import (
 	"testing"
 )
 
-func TestSample1(t *testing.T) {
+func TestSample(t *testing.T) {
+	sampleData := `1
+10
+100
+2024`
 
+	ans1, _ := Solve(sampleData)
+	if ans1 != 37327623 {
+		t.Fatalf("Mismatch! Expected 37327623 got %d", ans1)
+	}
 }
 
 func TestSample2(t *testing.T) {
+	sampleData := `1
+2
+3
+2024`
 
+	_, ans2 := Solve(sampleData)
+	if ans2 != 23 {
+		t.Fatalf("Mismatch! Expected 23 got %d", ans2)
+	}
 }
